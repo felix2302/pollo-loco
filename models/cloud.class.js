e6fbf39242f62ad5f9566775f199124be0766214
Felix2302 <felix.kirmse@gmail.com>
2024-08-01 17:24:49 +0200

    123

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -5,11 +5,7 @@ class Cloud extends MovableObject {
     height = 200;
     constructor() {
         super().loadImage('/img/5_background/layers/4_clouds/1.png');
-        this.x = Math.random() * 720;
-        this.y = 10;
-        this.width = 500;
-        this.height = 200;
     }
 
- 
+
 }
